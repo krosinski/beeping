0624cff2f67c33e3c0b7539afc27f0b44a00f9a5
chriss <krz.rosinski@gmail.com>
2014-06-02 19:53:54 +0200

    darwin support

--- beeping.sh
+++ beeping.sh
@@ -8,10 +8,20 @@ host=$1
 ping_timeout=1
 ping_sleep=3
 
+function beeeep {
+    if [ "$(uname)" == "Darwin" ]
+    then
+        afplay beep.wav
+    elif [ "$(uname)" == "Linux" ]
+    then
+        paplay beep.wav
+    fi
+}
+
 trap "exit" INT
 while [ "1" ]
 do
     ping -c 1 $host -W $ping_timeout 2>/dev/null >/dev/null && \
-        echo -n "." || (echo -n "X" && paplay beep.wav)
+        echo -n "." || (echo -n "X" && beeeep)
     sleep $ping_sleep
 done
